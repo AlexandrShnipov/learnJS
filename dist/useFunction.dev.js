802cd64740bf703814528d50d384cd2e3be3c406
AlexandrShnipov <sshnipov@gmail.com>
2022-04-15 22:35:12 +0300

    added useunction

--- dist/useFunction.dev.js
+++ dist/useFunction.dev.js
@@ -19,9 +19,54 @@ function checkDigitsPairsSum(num) {
   var sum1 = Number(str[0]) + Number(str[1]);
   var sum2 = Number(str[2]) + Number(str[3]);
   return sum1 == sum2;
-} //!Дана функция, проверяющая числа на простоту. 
-//С помощью приведенной функции найдите все простые числа в интервале от 1 до 100.
+} //!Дана функция getDigitsSum, которая параметром принимает целое число и возвращает сумму его цифр:
+//С помощью приведенной функции getDigitsSum найдите все года от 1 до 2030, сумма цифр которых равна 13.
 
+
+function getDigitsSum(num) {
+  var sum = 0;
+  var digits = String(num).split('');
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = digits[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var digit = _step.value;
+      sum += Number(digit);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  return sum;
+}
+
+var arrYears = function arrYears(yearStart, yearFinish) {
+  var arrSummNumbersYears = [];
+
+  for (var i = yearStart; i <= yearFinish; i++) {
+    if (getDigitsSum(i) === 13) {
+      arrSummNumbersYears.push(i);
+    }
+  }
+
+  return arrSummNumbersYears;
+};
+
+console.log(arrYears(1, 2030)); //!Дана функция, проверяющая числа на простоту. 
+//С помощью приведенной функции найдите все простые числа в интервале от 1 до 100.
 
 function isPrime(num) {
   for (var i = 2; i < num; i++) {
@@ -56,27 +101,27 @@ var numReverse = function numReverse(number) {
 
 var reverseArrayElements = function reverseArrayElements(arr) {
   var result1 = [];
-  var _iteratorNormalCompletion = true;
-  var _didIteratorError = false;
-  var _iteratorError = undefined;
+  var _iteratorNormalCompletion2 = true;
+  var _didIteratorError2 = false;
+  var _iteratorError2 = undefined;
 
   try {
-    for (var _iterator = arr[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-      var elem = _step.value;
+    for (var _iterator2 = arr[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+      var elem = _step2.value;
       var resultNumReverse = numReverse(elem);
       result1.push(resultNumReverse);
     }
   } catch (err) {
-    _didIteratorError = true;
-    _iteratorError = err;
+    _didIteratorError2 = true;
+    _iteratorError2 = err;
   } finally {
     try {
-      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
-        _iterator["return"]();
+      if (!_iteratorNormalCompletion2 && _iterator2["return"] != null) {
+        _iterator2["return"]();
       }
     } finally {
-      if (_didIteratorError) {
-        throw _iteratorError;
+      if (_didIteratorError2) {
+        throw _iteratorError2;
       }
     }
   }
@@ -85,3 +130,117 @@ var reverseArrayElements = function reverseArrayElements(arr) {
 };
 
 console.log(reverseArrayElements(nums)); // выведет [321, 654, 987]
+//!Даны два числа. Необходимо проверить, равна ли сумма цифр первого числа сумме цифр второго числа. Некий программист уже написал решение задачи, вот оно:
+//В чем недостатки приведенного кода? Переделайте код так, чтобы он был более удачным.
+
+var num1 = 234;
+var num2 = 531; // let digits1 = String(num1).split('');
+// let digitsSum1 = 0;
+// for (let digit1 of digits1) {
+// 	digitsSum1 += Number(digit1);
+// }
+// let digits2 = String(num1).split('');
+// let digitsSum2 = 0;
+// for (let digit2 of digits2) {
+// 	digitsSum2 += Number(digit2);
+// }
+// if (digitsSum1 == digitsSum2) {
+// 	alert('суммы цифр совпадают');
+// } else {
+// 	alert('суммы цифр не совпадают');
+// }
+
+var getSumNumbers = function getSumNumbers(num) {
+  var getSummArrNumbers = 0;
+  var arrNumbers = String(num).split('');
+  var _iteratorNormalCompletion3 = true;
+  var _didIteratorError3 = false;
+  var _iteratorError3 = undefined;
+
+  try {
+    for (var _iterator3 = arrNumbers[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+      var arrNumber = _step3.value;
+      getSummArrNumbers += Number(arrNumber);
+    }
+  } catch (err) {
+    _didIteratorError3 = true;
+    _iteratorError3 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion3 && _iterator3["return"] != null) {
+        _iterator3["return"]();
+      }
+    } finally {
+      if (_didIteratorError3) {
+        throw _iteratorError3;
+      }
+    }
+  }
+
+  return getSummArrNumbers;
+}; //console.log(getSumNumbers(123));
+
+
+var comparisonNumbers = function comparisonNumbers(numOne, numTwo) {
+  return (// if (getSumNumbers(numOne) === getSumNumbers(numTwo)) {
+    // 	console.log('сумма чисел равна')
+    // } else {
+    // 	console.log('сумма чисел не равна');
+    // }
+    getSumNumbers(numOne) === getSumNumbers(numTwo) ? console.log('сумма чисел равна') : console.log('сумма чисел не равна')
+  );
+};
+
+comparisonNumbers(num1, num2); //!Дан массив с числами. Необходимо перебрать этот массив с числами и вывести в консоль нетривиальные делители этих чисел (то есть исключая число 1 и само число). Некий программист уже написал решение задачи:
+//Программист не тестировал отдельно работу функции getDivisors, а написал весь код сразу и затем запустил его. При запуске оказалось, что код работает как-то не так. Видимо, в функции getDivisors была допущена какая-то ошибка.
+//Найдите и исправьте ошибку. Потестируйте отдельно функцию getDivisors, чтобы убедиться, что она работает корректно после вашей правки. После того, как вы убедитесь в корректности работы функции - проверьте полный код решения задачи.
+
+var arr2 = [12, 24, 35, 14]; // for (let num of nums) {
+// 	console.log(num + ': ' + getDivisors(num).join(', '));
+// }
+
+function getDivisors(num) {
+  var result = [];
+
+  for (var _i2 = 2; _i2 < num; _i2++) {
+    if (num % _i2 == 0) {
+      result.push(_i2);
+    }
+  }
+
+  return result;
+}
+
+console.log(getDivisors(10));
+
+var arrGetDivisors = function arrGetDivisors(arr) {
+  var arrResult = [];
+  var _iteratorNormalCompletion4 = true;
+  var _didIteratorError4 = false;
+  var _iteratorError4 = undefined;
+
+  try {
+    for (var _iterator4 = arr[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+      var num = _step4.value;
+      var divisors = getDivisors(num);
+      if (divisors.length > 0) arrResult = arrResult.concat(divisors);
+    }
+  } catch (err) {
+    _didIteratorError4 = true;
+    _iteratorError4 = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion4 && _iterator4["return"] != null) {
+        _iterator4["return"]();
+      }
+    } finally {
+      if (_didIteratorError4) {
+        throw _iteratorError4;
+      }
+    }
+  }
+
+  return arrResult;
+};
+
+console.log(arrGetDivisors(arr2));
--- useFunction.js
+++ useFunction.js
@@ -18,6 +18,31 @@ function checkDigitsPairsSum(num) {
 	return sum1 == sum2;
 }
 
+//!Дана функция getDigitsSum, которая параметром принимает целое число и возвращает сумму его цифр:
+//С помощью приведенной функции getDigitsSum найдите все года от 1 до 2030, сумма цифр которых равна 13.
+
+function getDigitsSum(num) {
+	let sum = 0;
+	let digits = String(num).split('');
+
+	for (let digit of digits) {
+		sum += Number(digit);
+	}
+	return sum;
+}
+
+const arrYears = (yearStart, yearFinish) => {
+	const arrSummNumbersYears = [];
+	for (let i = yearStart; i <= yearFinish; i++) {
+		if (getDigitsSum(i) === 13) {
+			arrSummNumbersYears.push(i);
+		}
+	}
+	return arrSummNumbersYears;
+}
+
+console.log(arrYears(1, 2030));
+
 //!Дана функция, проверяющая числа на простоту. 
 //С помощью приведенной функции найдите все простые числа в интервале от 1 до 100.
 function isPrime(num) {
@@ -44,20 +69,96 @@ console.log(primeNumbers);
 
 const nums = [123, 456, 789];
 
-const numReverse = number => 
-		Number(String(number).split('').reverse().join(''));
+const numReverse = number =>
+	Number(String(number).split('').reverse().join(''));
 //console.log(numReverse(123));
 
 const reverseArrayElements = (arr) => {
 	const result1 = [];
-		for (let elem of arr) {	
-			const resultNumReverse = numReverse(elem);
-			result1.push(resultNumReverse);
+	for (let elem of arr) {
+		const resultNumReverse = numReverse(elem);
+		result1.push(resultNumReverse);
 	}
 	return result1
 }
 
 console.log(reverseArrayElements(nums)); // выведет [321, 654, 987]
 
+//!Даны два числа. Необходимо проверить, равна ли сумма цифр первого числа сумме цифр второго числа. Некий программист уже написал решение задачи, вот оно:
+//В чем недостатки приведенного кода? Переделайте код так, чтобы он был более удачным.
+
+let num1 = 234;
+let num2 = 531;
+
+// let digits1 = String(num1).split('');
+// let digitsSum1 = 0;
+// for (let digit1 of digits1) {
+// 	digitsSum1 += Number(digit1);
+// }
+
+// let digits2 = String(num1).split('');
+// let digitsSum2 = 0;
+// for (let digit2 of digits2) {
+// 	digitsSum2 += Number(digit2);
+// }
+
+// if (digitsSum1 == digitsSum2) {
+// 	alert('суммы цифр совпадают');
+// } else {
+// 	alert('суммы цифр не совпадают');
+// }
+
+const getSumNumbers = num => {
+	let getSummArrNumbers = 0;
+	let arrNumbers = String(num).split('');
+	for (let arrNumber of arrNumbers) {
+		getSummArrNumbers += Number(arrNumber);
+	}
+	return getSummArrNumbers;
+}
+//console.log(getSumNumbers(123));
+
+const comparisonNumbers = (numOne, numTwo) =>
+	// if (getSumNumbers(numOne) === getSumNumbers(numTwo)) {
+	// 	console.log('сумма чисел равна')
+	// } else {
+	// 	console.log('сумма чисел не равна');
+	// }
+	getSumNumbers(numOne) === getSumNumbers(numTwo)
+		? console.log('сумма чисел равна')
+		: console.log('сумма чисел не равна')
+
+comparisonNumbers(num1, num2);
+
+//!Дан массив с числами. Необходимо перебрать этот массив с числами и вывести в консоль нетривиальные делители этих чисел (то есть исключая число 1 и само число). Некий программист уже написал решение задачи:
+//Программист не тестировал отдельно работу функции getDivisors, а написал весь код сразу и затем запустил его. При запуске оказалось, что код работает как-то не так. Видимо, в функции getDivisors была допущена какая-то ошибка.
+//Найдите и исправьте ошибку. Потестируйте отдельно функцию getDivisors, чтобы убедиться, что она работает корректно после вашей правки. После того, как вы убедитесь в корректности работы функции - проверьте полный код решения задачи.
+
+let arr2 = [12, 24, 35, 14];
+
+// for (let num of nums) {
+// 	console.log(num + ': ' + getDivisors(num).join(', '));
+// }
+
+function getDivisors(num) {
+	let result = [];
+	for (let i = 2; i < num; i++) {
+		if (num % i == 0) {
+			result.push(i);
+		}
+	}
+	return result;
+}
+console.log(getDivisors(10));
+
+const arrGetDivisors = arr => {
+	let arrResult = []
+	for (let num of arr) {
+		const divisors = getDivisors(num);
+		if (divisors.length > 0) arrResult = arrResult.concat(divisors);		
+	}
+	return arrResult;
+}
+console.log(arrGetDivisors(arr2));
 
 
